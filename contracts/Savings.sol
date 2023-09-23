994ed01e8fa7eadea4f7d038d3dbcd38ec32e861
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-09-22 22:05:41 -0400

    Added base solidity data structure

--- contracts/Savings.sol
+++ contracts/Savings.sol
@@ -2,14 +2,26 @@
 pragma solidity ^0.8.9;
 
 contract SavingsFactory {
+
+    struct TokenDistribution {
+      address token;
+      uint256 amount;
+    }
+
     address public immutable owner;
+    address[] public whitelistTokens;
+    TokenDistribution[] tokenDistribution;
 
     modifier onlyOwner {
       require(msg.sender == owner, "Only owner can call this function");
       _;
     }
 
-    constructor() {
+    constructor(address[] memory _whitelistTokens, TokenDistribution[] memory _tokenDistribution) {
       owner = msg.sender;
+      whitelistTokens = _whitelistTokens;
+      tokenDistribution = _tokenDistribution;
     }
+
+    
 }
