52637be3d35cf631f8cb1fb023aaacf69d44b025
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-09-23 03:09:22 -0400

    Added savings created event for graph

--- contracts/SavingsFactory.sol
+++ contracts/SavingsFactory.sol
@@ -22,29 +22,54 @@ interface KeeperRegistrarInterface {
 }
 
 contract SavingsFactory {
-    
+    event SavingsCreated(
+        address indexed creator,
+        address savings,
+        address[] whitelistTokens,
+        Savings.TokenDistribution[] tokenDistribution
+    );
+
     address public immutable owner;
     // LinkTokenInterface public immutable link;
     // KeeperRegistrarInterface public immutable registrar;
 
-    LinkTokenInterface public constant link = LinkTokenInterface(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-    KeeperRegistrarInterface public constant registrar = KeeperRegistrarInterface(0x57A4a13b35d25EE78e084168aBaC5ad360252467);
+    ISwapProxy swapProxy;
+    LinkTokenInterface public constant link =
+        LinkTokenInterface(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+    KeeperRegistrarInterface public constant registrar =
+        KeeperRegistrarInterface(0x57A4a13b35d25EE78e084168aBaC5ad360252467);
 
-    modifier onlyOwner {
-      require(msg.sender == owner, "Only owner can call this function");
-      _;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
 
-    constructor(/* LinkTokenInterface _link, KeeperRegistrarInterface _registrar */) {
+    constructor(
+        ISwapProxy _swapProxy /* , LinkTokenInterface _link, KeeperRegistrarInterface _registrar */
+    ) {
         owner = msg.sender;
+        swapProxy = _swapProxy;
         // link = _link;
         // registrar = _registrar;
     }
 
     function create(
-        address[] memory _whitelistTokens, Savings.TokenDistribution[] memory _tokenDistribution, uint96 amount
-    ) payable external {
-        Savings newSavings = new Savings(_whitelistTokens, _tokenDistribution);
+        address[] memory _whitelistTokens,
+        Savings.TokenDistribution[] memory _tokenDistribution,
+        uint96 amount
+    ) external payable {
+        Savings newSavings = new Savings(
+            swapProxy,
+            msg.sender,
+            _whitelistTokens,
+            _tokenDistribution
+        );
+        emit SavingsCreated(
+            msg.sender,
+            address(newSavings),
+            _whitelistTokens,
+            _tokenDistribution
+        );
 
         RegistrationParams memory params = RegistrationParams(
             "My savings",
