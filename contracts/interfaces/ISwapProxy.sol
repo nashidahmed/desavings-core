55c8cad7c64b777fe4138ac6498388535d8f625b
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-09-24 00:13:07 -0400

    Added multihop to interface

--- contracts/interfaces/ISwapProxy.sol
+++ contracts/interfaces/ISwapProxy.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.9;
 
 interface ISwapProxy {
-    function swapExactInputSingle(uint256 amountIn, address tokenIn, address tokenOut, address recipient) external payable returns (uint256 amountOut);
+    function swapExactInputSingle(
+      uint256 amountIn, 
+      address tokenIn, 
+      address tokenOut, 
+      address recipient
+    ) external payable returns (uint256 amountOut);
+
+    function swapExactInputMultihop(
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut,
+        address recipient
+    ) external returns (uint256 amountOut)
 }
